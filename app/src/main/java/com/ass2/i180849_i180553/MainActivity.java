3b44a8c0f3fa5959616c636701fe587098c98f09
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2022-10-14 21:39:04 +0500

    Initial Commits

---
+++ app/src/main/java/com/ass2/i180849_i180553/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ass2.i180849_i180553;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
